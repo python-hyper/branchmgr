0b6125c8e0b40d357156cd56e518b442053f5dac
Cory Benfield <lukasaoz@gmail.com>
2017-03-14 10:13:40 +0000

    Stub code for protecting branches

--- branchmgr/main.py
+++ branchmgr/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pprint
 import sys
@@ -10,6 +11,27 @@ from gidgethub.treq import GitHubAPI
 
 MY_TOKEN = os.environ['GHKEY']
 USER_AGENT = "Lukasa"
+
+
+def protection_data():
+    """
+    Get a dictionary representing the JSON required to protect a branch.
+    """
+    # TODO: This should be loadable from a YAML config file of some kind.
+    return {
+        "required_pull_request_reviews": {
+            "include_admins": False,
+        },
+        "required_status_checks": {
+            "include_admins": False,  # Enforce required status checks for repository administrators.
+            "strict": True,  # Require branches to be up to date before merging.
+            "contexts": ["continuous-integration/travis-ci"],  # The list of status checks to require in order to merge into this branch
+        },
+        "restrictions": {
+            "users": [],  # The list of user logins with push access
+            "teams": ["contributors"],  # The list of team slugs with push access
+        }
+    }
 
 
 class APIClient:
@@ -28,6 +50,17 @@ class APIClient:
             accept=accept_format(version="loki-preview")
         )
 
+    async def _set_branch_protection(self, owner, reponame, branch, status):
+        """
+        Given a branch on a repository, sets the branch protection status to
+        status. Status must be a dictionary.
+        """
+        encoded_status = json.dumps(status)
+        return await self._gh.put(
+            f"/repos/{owner}/{reponame}/branches/{branch}/protection",
+            accept=accept_format(version="loki-preview")
+        )
+
     async def branch_requires_review(self, owner, reponame, branch):
         """
         Returns whether or not a given branch requires pull request review
@@ -41,6 +74,13 @@ class APIClient:
             return False
         else:
             return "required_pull_request_reviews" in response
+
+    async def protect_branch(self, owner, reponame, branch):
+        """
+        Protects a given branch.
+        """
+        protection_status = protection_data()
+        self._set_branch_protection(owner, reponame, branch, protection_status)
 
 
 async def async_main():
